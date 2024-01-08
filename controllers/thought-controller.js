ace88ef33f0bcaa19e0d3d9b1fd50372bce56dfe
Arturo <arturodamianvargas1996@gmail.com>
2024-01-07 20:44:35 -0600

    Added final coding touches and module exports for thought controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -78,4 +78,63 @@ updateThought({ params, body }, res) {
     .catch(err) => res.json(err));
     },
 
+    // Delete Thought //
+deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+    .then((dbThoughtData) => {
+    if (!dbThoughtData) {
+    return res.status(404).json({ message: "No thought with this id!" });
+    }
+    
+    // Remove thought id from user //
+    return User.findOneAndUpdate(
+    {thoughts: params.id },
+    { $pull: {thoughts: params.id } },
+    {new: true}
+    );
+    })
+    .then ((dbUserData) => {
+    if (!dbUserData) {
+    return res
+    .status(404)
+    .json({ message: "Thought created byt no user with this id!" });
+    }
+    res.json ({message: "Thought sucessfully deleted!" });
+    })
+    .catch ((err) => res.json(err));
+    },
+    
+    // Add reaction //
+
+addReaction({ params, body }, res) {
+    Thought.findOneandUpdate(
+    {_id: params.thoughtId },
+    { $addToSet: { reactions: body } },
+    {new: true, runValidators: true }
+    )
+    .then((dbThoughtData) => {
+    if (!dbThoughtData) {
+    res.status(404).json({ message: "No thought with this id" });
+    return;
+    }
+    res.json(dbThoughtData);
+    })
+    .catch((err) => res.json(err));
+    },
+
+    // Delete reaction //
+removeReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+    {_id: params.thoughtId },
+    {pull: {reactions: { reactionId: params.reactionId } } },
+    { new: true }
+    )
+    .then((dbThoughtData) => res.json(dbThoughtData))
+    .catch((err) => res.json(err));
+    
+    },
+    };
+
+    
+module.exports = thoughtController;
     
