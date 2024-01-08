75debc75d6f46666d1bada33819ce70f14cb0ce9
Arturo <arturodamianvargas1996@gmail.com>
2024-01-08 01:10:14 -0600

    Added code for User.js file

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const {
+
+getAllThought,
+getThoughtById,
+createThought,
+updateThought,
+deleteThought,
+addReaction,
+removeReaction,
+removeReaction,
+
+} = require("../../controllers/thought-controller");
+
+// Api Thoughts //
+router.route("/").get(getAllThought).post(createThought);
+
+// api thoughts/:id //
+router
+
+.route("/:id")
+.get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought);
+
+// Api/thoughts/"thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /Api/thoughts/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
